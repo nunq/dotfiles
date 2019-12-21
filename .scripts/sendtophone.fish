e2be2375a9371c476c5b99c5c70868deb3c18d6f
hyphenc <46054695+hyphenc@users.noreply.github.com>
2019-12-21 14:01:39 +0100

    fix sendtophone.fish

--- .scripts/sendtophone.fish
+++ .scripts/sendtophone.fish
@@ -1,7 +1,6 @@
 #!/usr/bin/fish
 # vim: syntax=sh
 set TEXT (tail /run/user/(id -u)/clipmenu.5.(id -un)/line_cache_clipboard | sort -r | sed 's/^[0-9]* //gi' | dmenu -p "push what?" -l 5 | jq -sRr @uri)
-curl "$API""$TEXT" > /dev/null 2>&1
 
 if echo "$TEXT" | grep -q "^http"
   set APICALL "https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?deviceId=group.android&apikey=""$JOIN_APIKEY""&url="
