02274f6dbc5bf646ba21ade35455c416ef6c8ff6
hyphenc <46054695+hyphenc@users.noreply.github.com>
2020-03-31 22:01:27 +0200

    update scripts, fix dwm bar

--- .scripts/sendtophone.fish
+++ .scripts/sendtophone.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 # vim: syntax=sh
-set TEXT (tail /run/user/(id -u)/clipmenu.5.(id -un)/line_cache_clipboard | tac | sed 's/^[0-9]* //gi' | dmenu -p "push what?" -l 5 | jq -sRr @uri)
+set TEXT (tail /run/user/(id -u)/clipmenu.6.(id -un)/line_cache | tac | sed 's/^[0-9]* //gi' | dmenu -p "push what?" -l 5 | jq -sRr @uri)
 
 if echo "$TEXT" | grep -q "^http"
   set APICALL "https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?deviceId=group.android&apikey=""$JOIN_APIKEY""&url="
